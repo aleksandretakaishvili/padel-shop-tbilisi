1a2a9fdea4d33e7f35724ac48f10420eece69e7c
aleksandretakaishvili <aleqsandretakaishvili@gmail.com>
2023-11-09 05:48:53 +0400

    sa

--- src/Assets/Components/Blocks/Footer/Footer.js
+++ src/Assets/Components/Blocks/Footer/Footer.js
@@ -2,35 +2,66 @@ import React from "react";
 import "./Footer.css";
 import { PiCopyrightLight } from "react-icons/pi";
 import { TfiInstagram } from "react-icons/tfi";
+import { AiFillInstagram } from "react-icons/ai";
+import { BiLogoTiktok } from "react-icons/bi";
+import { BsFillArrowDownLeftSquareFill } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
 
 function Footer() {
   return (
     <>
       <footer>
-        <div className="footer-nav">
-          <h1>გვერდები</h1>
-          <ul>
-            <li>
-              <NavLink to="/" activeClassName="active">
-                მთავარი
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/product" activeClassName="active">
-                პროდუქცია
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/what-is-padel" activeClassName="active">
-                რა არის პადელი ?
-              </NavLink>
-            </li>
-          </ul>
+        <div className="left-side-container">
+          <div className="left-side-item">
+            <h1>ნავიგაცია</h1>
+            <div className="line"></div>
+            <ul>
+              <li>
+                <NavLink to="/" activeClassName="active">
+                  მთავარი
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/product" activeClassName="active">
+                  პროდუქცია
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/what-is-padel" activeClassName="active">
+                  რა არის პადელი ?
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="left-side-item">
+            <h1>ჩვენს შესახებ</h1>
+            <div className="line"></div>
+            <p>პადელის პირველი ონლაინ მაღაზია საქართველოში.</p>
+          </div>
+          <div className="left-side-item">
+            <h1>სოციალური ქსელები</h1>
+            <div className="line"></div>
+            <div className="footer-social-icons">
+              <a href="">
+                <AiFillInstagram className="instagram" />
+              </a>
+              <a href="">
+                <BiLogoTiktok className="tiktok" />
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="about-us">
-          <h1>ჩვენს შესახებ</h1>
-          <p>პადელის პირველი ონლაინ მაღაზია საქართველოში.</p>
+        <div className="right-side-container">
+          <div className="right-side-header">
+            <h2>
+              მეტი ინფორმაციისთვის <br></br>
+              შეავსეთ <BsFillArrowDownLeftSquareFill />
+            </h2>
+          </div>
+          <div className="form-container">
+            <label htmlFor="">სახელი</label>
+            <input type="text" />
+          </div>
         </div>
       </footer>
       <div className="bottom-footer">
